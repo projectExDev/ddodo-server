a8e31d8e56ff7d171146c4a76bc96d053e478a70
s-yeong <syxxng25@gmail.com>
2022-09-18 17:11:55 +0900

    init

---
+++ src/main/java/com/untitled/DDODO/DdodoApplication.java
@@ -0,0 +1,13 @@
+package com.untitled.DDODO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/untitled/DDODO/DdodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.untitled.DDODO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
